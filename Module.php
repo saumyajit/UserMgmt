14c554d965865be1aa1876554a83c6b559aff6fa
Saumyajit <saumyajit@users.noreply.github.com>
2026-08-21 13:30:23 +0530

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\mwcalpro;
+
+use Zabbix\Core\CModule;
+use APP;
+use CMenuItem;
+
+class Module extends CModule {
+
+    public function init(): void {
+        APP::Component()->get('menu.main')
+            ->findOrAdd(_('Reports'))
+                ->getSubmenu()
+                    ->insertAfter(_('Notification'),
+                        (new CMenuItem(_('Maintenance Calendar')))->setAction('maintenance.calendar')
+                    );
+    }
+}
