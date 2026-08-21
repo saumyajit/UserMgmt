49ef3bf4eea487181d74f220252c81e846576570
Saumyajit <saumyajit@users.noreply.github.com>
2026-08-21 13:53:59 +0530

    Refactor HTML structure and improve readability

--- views/user.policy.execute.php
+++ views/user.policy.execute.php
@@ -1,14 +1,12 @@
 <?php declare(strict_types = 1);
 
-/**
- * @var CView $this
- * @var array $data
- */
+/** @var CView $this */
+/** @var array $data */
 
 $processed = $data['processed'] ?? [];
 $errors = $data['errors'] ?? [];
-$dry_run = (int) ($data['dry_run'] ?? 1);
 $operation = $data['operation'] ?? '';
+$dry_run = (int) ($data['dry_run'] ?? 1);
 ?>
 
 <?php if ($dry_run): ?>
@@ -17,46 +15,40 @@ $operation = $data['operation'] ?? '';
 	</div>
 <?php else: ?>
 	<div class="msg-good">
-		<?= htmlspecialchars(
-			sprintf(_('%s operation completed.'), ucfirst($operation)),
-			ENT_QUOTES
-		) ?>
+		<?= htmlspecialchars(sprintf(_('%s operation completed.'), ucfirst($operation)), ENT_QUOTES) ?>
 	</div>
 <?php endif; ?>
 
 <?php if ($processed): ?>
-	<div class="table-scroll">
-		<table class="list-table">
-			<thead>
-				<tr>
-					<th><?= _('User ID') ?></th>
-					<th><?= _('Username') ?></th>
-					<th><?= _('Operation') ?></th>
-					<th><?= _('Status') ?></th>
-				</tr>
-			</thead>
-
-			<tbody>
-			<?php foreach ($processed as $item): ?>
-				<tr>
-					<td><?= htmlspecialchars($item['userid'], ENT_QUOTES) ?></td>
-					<td><?= htmlspecialchars($item['username'], ENT_QUOTES) ?></td>
-					<td><?= htmlspecialchars($item['operation'], ENT_QUOTES) ?></td>
-					<td><?= htmlspecialchars($item['status'], ENT_QUOTES) ?></td>
-				</tr>
-			<?php endforeach; ?>
-			</tbody>
-		</table>
-	</div>
+	<table class="list-table">
+		<thead>
+			<tr>
+				<th><?= _('User ID') ?></th>
+				<th><?= _('Username') ?></th>
+				<th><?= _('Operation') ?></th>
+				<th><?= _('Status') ?></th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($processed as $item): ?>
+			<tr>
+				<td><?= htmlspecialchars($item['userid'], ENT_QUOTES) ?></td>
+				<td><?= htmlspecialchars($item['username'], ENT_QUOTES) ?></td>
+				<td><?= htmlspecialchars($item['operation'], ENT_QUOTES) ?></td>
+				<td><?= htmlspecialchars($item['status'], ENT_QUOTES) ?></td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
 <?php endif; ?>
 
 <?php if ($errors): ?>
 	<div class="msg-bad">
 		<strong><?= _('Warnings and errors') ?></strong>
 		<ul>
-			<?php foreach ($errors as $error): ?>
-				<li><?= htmlspecialchars($error, ENT_QUOTES) ?></li>
-			<?php endforeach; ?>
+		<?php foreach ($errors as $error): ?>
+			<li><?= htmlspecialchars($error, ENT_QUOTES) ?></li>
+		<?php endforeach; ?>
 		</ul>
 	</div>
 <?php endif; ?>
